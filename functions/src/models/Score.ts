d892381598fcf401e52c05d4d68944de6bf3a965
Tyson <tywalk10@gmail.com>
2021-12-07 11:50:30 -0500

    score

--- functions/src/models/Score.ts
+++ functions/src/models/Score.ts
@@ -3,8 +3,7 @@ import { ObjectId } from "mongodb";
 export default interface Score {
   _id?: ObjectId;
   uid: string;
-  date: string;
-  time: number;
   avatar: string;
   username: string;
+  score: number;
 }
